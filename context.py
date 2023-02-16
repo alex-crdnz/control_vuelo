96367be9131ce935e10bfa79507fff35f204c98c
Andrés Cárdenas de la Cruz <andres.cardenas@lefort.io>
2023-02-16 17:27:07 -0600

    push inicial del proyecto con estructura de la base de datos

--- context.py
+++ context.py
@@ -12,7 +12,5 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET")
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 300
 db = SQLAlchemy(app, session_options={"expire_on_commit": False})
 api = Api(app)
