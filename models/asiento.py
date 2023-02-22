f67965dd13dd9a77986f8650ff66eee00ba1a12d
Andrés Cárdenas de la Cruz <andres.cardenas@lefort.io>
2023-02-22 10:41:17 -0600

    actualizacion_estructura_bd

--- models/asiento.py
+++ models/asiento.py
@@ -8,21 +8,27 @@ class Asiento(db.Model):
     __tablename__ = 'asiento'
     id = db.Column(db.Integer, primary_key=True)
     id_vuelo = db.Column(db.Integer, db.ForeignKey('vuelo.id'), nullable=False)
+    clave_reservacion = db.Column(db.Integer, db.ForeignKey('reservacion.id'), nullable=True)
     clave_asiento = db.Column(db.String(30), nullable=False)
+    ventana = db.Column(db.String(30), nullable=False)
+    pasillo = db.Column(db.String(30), nullable=False)
     clase = db.Column(db.String(30), nullable=False)
-    equipaje = db.Column(db.String(30), nullable=True)
-    costo = db.Column(db.String(70), nullable=False)
-    status = db.Column(db.String(30), nullable=False)
+    equipaje = db.Column(db.String(30), nullable=False)
+    disponible = db.Column(db.Boolean, nullable=True)
+    costo_base = db.Column(db.String(30), nullable=False)
     created = db.Column(db.DateTime, nullable=False)
     
 
 
 
-    def __init__(self, id_vuelo, clave_asiento, clase, equipaje, costo, status, created):
+    def __init__(self, id_vuelo, clave_reservacion, clave_asiento, ventana, pasillo, clase, equipaje, disponible, costo, created):
         self.id_vuelo = id_vuelo,
+        self.clave_reservacion = clave_reservacion,
         self.clave_asiento = clave_asiento,
+        self.ventana = ventana,
+        self.pasillo = pasillo,
         self.clase = clase,
         self.equipaje = equipaje,
+        self.disponible = disponible,
         self.costo = costo,
-        self.status = status,
         self.created = created
